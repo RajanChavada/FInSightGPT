2db191f506a03baeb0d0444fb23fa01e666aa238
Jimmy Chavada <rchavada@uwo.ca>
2025-06-20 22:51:04 -0400

    Created linear regression model

--- finsightgpt/tailwind.config.js
+++ finsightgpt/tailwind.config.js
@@ -1,4 +1,12 @@
-content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
